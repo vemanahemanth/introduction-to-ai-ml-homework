4cd05e6f3c025a7fcbbe64b8a3e81d47f37272b3
Vemana Hemanth <vemanahb.eai24@chanakyauniversity.edu.in>
2025-08-22 23:14:03 +0530

    Add files via upload

---
+++ homework1b.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+X = np.array([1, 2, 3, 4, 5], dtype=float)
+Y = np.array([5, 8, 11, 14, 17], dtype=float)
+
+
+m = np.cov(X, Y, bias=True)[0][1] / np.var(X)
+c = np.mean(Y) - m * np.mean(X)
+
+print("Slope (m):", m)
+print("Intercept (c):", c)
+print("Prediction for x=6:", m*6 + c)
