ec69d813e60d44e93d9a7e29bbb7c286305500e4
Vemana Hemanth <vemanahb.eai24@chanakyauniversity.edu.in>
2025-08-22 22:52:54 +0530

    Add files via upload

---
+++ homework1A.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Grade", grade)
